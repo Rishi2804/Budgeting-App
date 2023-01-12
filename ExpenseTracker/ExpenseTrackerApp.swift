3482f7c1d87dedf0794fd6e3de6c206f995c8d53
Rishi2804 <77213736+Rishi2804@users.noreply.github.com>
2023-01-11 20:56:14 -0500

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Rishi Atukuri on 2023-01-11.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
